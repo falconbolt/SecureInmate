ad3ce3578fc0ead00d21a720a7e96fb8cd3d5854
BusolaAM <busolaatinsola@gmail.com>
2023-05-30 09:36:16 +0100

    models-update

--- models/Inmates.js
+++ models/Inmates.js
@@ -7,11 +7,115 @@ const InmateSchema = new mongoose.Schema(
     },
     offence_cartegory : {
       type: String,
-      required: false,
+      required: true,
     },
-     endDate: {
+     gender : {
+      type: String,
+      required: true,
+    },
+     date_of_birth : {
+      type: String,
+      required: true,
+    },
+     ethnicity : {
+      type: String,
+      required: true,
+    },
+     social_security : {
+      type: String,
+      required: true,
+    },
+     phone_number : {
+      type: String,
+      required: true,
+    },
+     height : {
+      type: String,
+      required: true,
+    },
+     weight : {
+      type: String,
+      required: true,
+    },
+     eye_color : {
+      type: String,
+      required: true,
+    },
+     hair_color : {
+      type: String,
+      required: true,
+    },
+     scar_ : {
+      type: String,
+      required: true,
+    },
+     medical_condition : {
+      type: String,
+      required: true,
+    },
+     disability : {
+      type: String,
+      required: true,
+    },
+     id_number : {
+      type: String,
+      required: true,
+    },
+     bookingDate : {
       type: Date,
-      required: false,
+      required: true,
+    },
+     booking_officer : {
+      type: String,
+      required: true,
+    },
+     arrest_oficer : {
+      type: String,
+      required: true,
+    },
+     booking_time : {
+      type: String,
+      required: true,
+    },
+     arresting_agency : {
+      type: String,
+      required: true,
+    },
+     arrest_location : {
+      type: String,
+      required: true,
+    },
+     arrest_time : {
+      type: String,
+      required: true,
+    },
+     arrestDate : {
+      type: Date,
+      required: true,
+    },
+     verdict : {
+      type: String,
+      required: true,
+    },
+     sentencing_court : {
+      type: String,
+      required: true,
+    },
+     belongings : {
+      type: String,
+      required: true,
+    },
+     ImagePath : {
+      type: String,
+      required: true,
+    },
+     fingerPrint : {
+      type: String,
+      required: true,
+    },
+      endDate : {
+      type: Date,
+      required: true,
     },
     isActive: {
       type: String,
